cb9fb1d9478e82da349ae0158462d12d126dcdea
NancyZW <zhangwen@shanghaitech.edu.cn>
2020-02-11 20:06:03 +0800

    Q16

--- Q16/Q16.py
+++ Q16/Q16.py
@@ -13,10 +13,27 @@ class Solution:
         for i in range(exponent):
             ans = base*ans
         return ans
+    def Power2(self, base, exponent):
+        if base == 0:
+            return 0
+        if exponent == 0:
+            return 1
+        if exponent < 0:
+            base = 1/base
+            exponent = -exponent
+        if exponent & 1 == 1:
+            ans = self.Power2(base, exponent>>1) * self.Power2(base, exponent>>1)*base
+        else:
+            ans = self.Power2(base,exponent>>1)*self.Power2(base,exponent>>1)
+        return ans
+
+
+
 
 if __name__ == "__main__":
     base = 4
     exponent = -1
     sol = Solution()
     ans = sol.Power(base,exponent)
+    ans = sol.Power2(base,exponent)
     print(ans)
